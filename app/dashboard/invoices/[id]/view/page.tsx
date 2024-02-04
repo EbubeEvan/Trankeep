4d8619d6fd8e61a86ea0d53cb2146ef563cbae69
EbubeEvan <ebubemmayie@gmail.com>
2024-02-04 16:06:34 +0100

    fixed view page

--- app/dashboard/invoices/[id]/view/page.tsx
+++ app/dashboard/invoices/[id]/view/page.tsx
@@ -1,43 +1,11 @@
-"use client";
-
-import { useState } from "react";
-import Image from "next/image";
-import { EyeIcon, ArrowDownIcon } from "@heroicons/react/24/outline";
 import ViewForm from "@app/ui/invoices/view-form";
 
 const page = () => {
-  const [pdfurl, setPdfUrl] = useState("");
-  const [generate, setGenerate] = useState(false);
-
   return (
     <main className="flex h-full flex-col items-center justify-center">
-      <div>
-        {!pdfurl ? (
-          <div>
-            <button className="rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400">
-              {!generate ? "Generate invoice" : "Generating..."}
-            </button>
-          </div>
-        ) : (
-          <div>
-            <div className="border border-gray-400 p-5 rounded-t-md flex justify-center">
-              <Image
-                src="/pdf-icon.png"
-                alt="pdf icon"
-                width={150}
-                height={150}
-              />
-              </div>
-              <div className="bg-blue-600 flex justify-between w-full h-full border-gray-400 rounded-b-md p-2">
-                <EyeIcon width={30} className="text-white cursor-pointer "/>
-                <ArrowDownIcon width={30} className="text-white cursor-pointer "/>
-              </div>
-               <ViewForm/>
-          </div>
-        )}
-      </div>
+      <ViewForm/>
     </main>
-  );
+  )
 };
 
 export default page;
--- app/ui/invoices/view-form.tsx
+++ app/ui/invoices/view-form.tsx
@@ -1,12 +1,56 @@
-import React from 'react'
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import { EyeIcon, ArrowDownIcon } from "@heroicons/react/24/outline";
 
 const ViewForm = () => {
-  return (
-    <form action="" className='flex gap-x-[3rem] gap-y-[2rem] flex-col md:flex-row mt-5'>
-        <button className='bg-blue-500 text-white p-3 rounded-md'>Send as Email</button>
-        <button type='submit' className='bg-blue-500 text-white p-3 rounded-md'>Generate reciept</button>
-    </form>
-  )
-}
+  const [pdfurl, setPdfUrl] = useState("a");
+  const [generate, setGenerate] = useState(false);
 
-export default ViewForm
+  return (
+    <div>
+      {!pdfurl ? (
+        <div>
+          <form action="">
+            <button className="rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400">
+              {!generate ? "Generate invoice" : "Generating..."}
+            </button>
+          </form>
+        </div>
+      ) : (
+        <div>
+          <div className="border border-gray-400 p-5 rounded-t-md flex justify-center">
+            <Image
+              src="/pdf-icon.png"
+              alt="pdf icon"
+              width={150}
+              height={150}
+              priority={true}
+            />
+          </div>
+          <div className="bg-blue-600 flex justify-between w-full h-full border-gray-400 rounded-b-md p-2">
+            <EyeIcon width={30} className="text-white cursor-pointer " />
+            <ArrowDownIcon width={30} className="text-white cursor-pointer " />
+          </div>
+          <form
+            action=""
+            className="flex gap-x-[3rem] gap-y-[1.5rem] flex-col md:flex-row mt-5"
+          >
+            <button className="bg-blue-500 text-white p-3 rounded-md">
+              Send as Email
+            </button>
+            <button
+              type="submit"
+              className="bg-blue-500 text-white p-3 rounded-md"
+            >
+              Generate reciept
+            </button>
+          </form>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ViewForm;
