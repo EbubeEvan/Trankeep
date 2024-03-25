01c49de5e282e7df23e4df55c84f980f2789d182
EbubeEvan <ebubemmayie@gmail.com>
2024-03-25 12:05:14 +0100

    decluttered side nav

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 import NavLinks from "@/app/ui/dashboard/nav-links";
 import { PowerIcon } from "@heroicons/react/24/outline";
 import { signOut } from '@/auth';
-import Image from "next/image";
 
 const SideNav = () => {
   return (
